9d3d4b8a93608a383bbeecc385a6e3ba3a478d78
zczb123 <zczb123@gmail.com>
2025-02-09 22:25:52 +0900

    초기설정

--- src/pages/ApplyPage.js
+++ src/pages/ApplyPage.js
@@ -7,17 +7,25 @@ const ApplyPage = () => {
     const bannerStyle = {
         background: "url('/files/attach/images/106/0e1b3587a1741f541b78bbd83d40e0c5.jpg') no-repeat center center",
         backgroundSize: "cover",
-        height: "500px",
+        height: "400px",
         borderRadius: "20px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+        width: "94vw", // 전체 너비보다 조금 줄임
+        marginLeft: "calc(50% - 47vw)", // 95vw의 절반은 47.5vw: 중앙 정렬 효과 적용
     };
 
-    // 신청 옵션 박스 인라인 스타일
     const optionBoxStyle = {
         background: "url('/files/attach/images/sub/apply_bg.jpg') no-repeat center center",
         backgroundSize: "cover",
         borderRadius: "10px",
         boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
         padding: "20px",
+        height: "650px",
+        margin: "30px 0", // 위아래 간격 추가
     };
 
     return (
@@ -30,37 +38,49 @@ const ApplyPage = () => {
             </div>
 
             <div className="applyOptionsContainer">
+                {/* 대면 수거 신청 옵션 */}
                 <div className="applyOptionBox" style={optionBoxStyle}>
                     <h2 className="applyOptionTitle">대면 수거 신청</h2>
-                    <p className="applyOptionDesc">
-                        대면으로 수거 신청 해주시면 원하시는 날짜에 업체 기사님께서 가정에 직접 방문하여
-                        버릴 물건들 포장 및 수거까지 도와드립니다.
-                    </p>
+                    <div className="applyOptionSection">
+                        <h3 className="applyOptionSectionTitle">대면 수거란?</h3>
+                        <p className="applyOptionDesc">
+                            대면으로 수거 신청 해주시면 원하시는 날짜에 업체 기사님께서 가정에 직접 방문하여
+                            버릴 물건들 포장 및 수거까지 도와드립니다.
+                        </p>
+                    </div>
                     <div className="applyOptionRecommendation">
-                        <p className="applyOptionRecTitle">이런 분들께 추천 드립니다!</p>
-                        <ul>
-                            <li>버릴 물건이 많아 혼자서 포장이 힘드신 분들</li>
-                            <li>비닐, 박스 등 포장 제품이 구비가 안되신 분들</li>
-                            <li>수거 전 날 기사님과 원활히 시간 조정 가능 하신 분들</li>
-                        </ul>
+                        <div className="applyOptionRecWrapper">
+                            <p className="applyOptionRecTitle">이런 분들께 추천 드립니다!</p>
+                            <ul className="applyOptionRecList">
+                                <li>버릴 물건이 많아 혼자서 포장이 힘드신 분들</li>
+                                <li>비닐, 박스 등 포장 제품이 구비가 안되신 분들</li>
+                                <li>수거 전 날 기사님과 원활히 시간 조정 가능 하신 분들</li>
+                            </ul>
+                        </div>
                     </div>
                     <Link to="/apply1" className="applyOptionBtn">
                         대면 수거 신청하기
                     </Link>
                 </div>
 
+                {/* 비대면 수거 신청 옵션 */}
                 <div className="applyOptionBox" style={optionBoxStyle}>
                     <h2 className="applyOptionTitle">비대면 수거 신청</h2>
-                    <p className="applyOptionDesc">
-                        비대면으로 수거 신청 해주시면 원하시는 날짜에 맞게 포장하여 문 밖에 내 놓아주시면 기사님께서
-                        문 앞에 놓인 물건을 수거 후 무게 사진 전송 및 송금하는 방식입니다.
-                    </p>
+                    <div className="applyOptionSection">
+                        <h3 className="applyOptionSectionTitle">비대면 수거란?</h3>
+                        <p className="applyOptionDesc">
+                            비대면으로 수거 신청 해주시면 원하시는 날짜에 맞게 포장하여 문 밖에 내 놓아주시면 기사님께서
+                            문 앞에 놓인 물건을 수거 후 무게 사진 전송 및 송금하는 방식입니다.
+                        </p>
+                    </div>
                     <div className="applyOptionRecommendation">
-                        <p className="applyOptionRecTitle">이런 분들께 추천 드립니다!</p>
-                        <ul>
-                            <li>혼자서 포장 가능 하신 분들</li>
-                            <li>수거 전 날 기사님과 시간 조정이 어려우신 분들</li>
-                        </ul>
+                        <div className="applyOptionRecWrapper">
+                            <p className="applyOptionRecTitle">이런 분들께 추천 드립니다!</p>
+                            <ul className="applyOptionRecList">
+                                <li>혼자서 포장 가능 하신 분들</li>
+                                <li>수거 전 날 기사님과 시간 조정이 어려우신 분들</li>
+                            </ul>
+                        </div>
                     </div>
                     <Link to="/apply2" className="applyOptionBtn">
                         비대면 수거 신청하기
--- src/pages/GuidePage.js
+++ src/pages/GuidePage.js
@@ -1,31 +1,46 @@
 import React from "react";
 import "swiper/css";
 import "swiper/css/autoplay";
-import "../styles/guidePage.css"
+import "../styles/guidePage.css";
 import Section3 from "../components/Section3";
 
 const GuidePage = () => {
+    // 상단 배너 인라인 스타일 (새 이미지와 디자인 적용)
+    const bannerStyle = {
+        background: "url('/files/attach/images/106/893ada4896cce8c524983e6025a10144.jpg') no-repeat center center",
+        backgroundSize: "cover",
+        height: "400px",
+        borderRadius: "20px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+        width: "94vw", // 전체 너비보다 조금 줄임
+        marginLeft: "calc(50% - 47vw)", // 중앙 정렬 효과 적용
+    };
+
     return (
-        <div className="guide_page">
+        <div className="guidePageContainer">
             {/* 상단 배너 */}
-            <div className="sub_top_banner">
-                <div className="sub_top_banner_content">
-                    <h2 className="title">이용안내</h2>
-                    <div className="hr-line"></div>
+            <div className="guideBanner" style={bannerStyle}>
+                <div className="guideBannerContent">
+                    <h2 className="guideBannerTitle">이용안내</h2>
+                    <div className="guideBannerUnderline"></div>
                 </div>
             </div>
 
             {/* 매입 단가 섹션 */}
-            <div className="guide_price_wrap">
-                <h3 className="sub_title">매입 단가</h3>
-                <div className="price_list">
-                    <div className="price_item">
-                        <h4 className="price_title">의류, 신발, 잡화, 가방</h4>
+            <div className="guidePriceWrap">
+                <h3 className="guideSubTitle">매입 단가</h3>
+                <div className="priceList">
+                    <div className="priceItem">
+                        <h4 className="priceTitle">의류, 신발, 잡화, 가방</h4>
                         <p>20Kg 미만 → <b>300원</b></p>
                         <p>20Kg 이상 → <b>500원</b></p>
                     </div>
-                    <div className="price_item">
-                        <h4 className="price_title">이불, 커튼, 카펫</h4>
+                    <div className="priceItem">
+                        <h4 className="priceTitle">이불, 커튼, 카펫</h4>
                         <p>*kg → <b>300원</b></p>
                     </div>
                 </div>
@@ -35,9 +50,9 @@ const GuidePage = () => {
             <Section3 />
 
             {/* 수거 불가능 품목 */}
-            <div className="guide_no_collection_wrap">
-                <h3 className="sub_title">수거 불가능 품목</h3>
-                <div className="no_collection_list">
+            <div className="guideNoCollectionWrap">
+                <h3 className="guideSubTitle">수거 불가능 품목</h3>
+                <div className="noCollectionList">
                     <p><b>하자가 있는 물품:</b> 찢어짐, 큰 오염, 인조 가죽 날림 등</p>
                     <p><b>기타 불가능 신발류:</b> 바퀴달린 신발, 장화, 고무신 등</p>
                     <p><b>기타 불가능 이불 및 잡화류:</b> 솜 이불, 토퍼, 라텍스 등</p>
@@ -45,10 +60,10 @@ const GuidePage = () => {
             </div>
 
             {/* 1:1 문의 유도 */}
-            <div className="guide_contact_wrap">
+            <div className="guideContactWrap">
                 <h3>수거 가능, 불가능 품목이 헷갈린다면?</h3>
                 <p>언제든지 1:1 문의를 남겨주세요.</p>
-                <a href="/inquiry" className="contact_btn">1:1 문의 바로가기</a>
+                <a href="/inquiry" className="contactBtn">1:1 문의 바로가기</a>
             </div>
         </div>
     );
