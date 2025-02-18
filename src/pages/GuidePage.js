b142665ab0fef055c2b0297bc87b601b4dcfa321
zczb123 <zczb123@gmail.com>
2025-02-18 17:19:54 +0900

    초기설정

--- src/pages/GuidePage.js
+++ src/pages/GuidePage.js
@@ -3,6 +3,7 @@ import "swiper/css";
 import "swiper/css/autoplay";
 import "../styles/guidePage.css";
 import Section3 from "../components/Section3";
+import Section8 from "../components/Section8";
 
 const GuidePage = () => {
     // 상단 배너 인라인 스타일 (새 이미지와 디자인 적용)
@@ -46,16 +47,60 @@ const GuidePage = () => {
                 </div>
             </div>
 
+            <b>
+                <div className="guide_check_wrap mt-30 mb-10" data-anim-delay="0" data-anim-type="fade-in-up">
+                    <div className="guide-check-box">
+                        <i className="ri-checkbox-circle-line guide-highlight fs-30 pr-10 lh-1"></i>
+                        <span className="guide-text-large guide-weight-medium">
+                            이용자분들의 편의를 위해 <b className="guide-highlight">최소 수거 수량에 제한이 없습니다.</b>
+                        </span>
+                    </div>
+                </div>
+                <div className="guide_check_wrap" data-anim-delay="0" data-anim-type="fade-in-up">
+                    <div className="guide-check-box">
+                        <i className="ri-checkbox-circle-line guide-highlight fs-30 pr-10 lh-1"></i>
+                        <span className="guide-text-large guide-weight-medium">
+                            무역 업체 단가 변동에 따라 현 매입 단가도 변동 될 수 있으며 <b className="guide-highlight">항상 최고가로 매입 단가 책정 하겠습니다.</b>
+                        </span>
+                    </div>
+                </div>
+            </b>
+
             {/* 수거 가능 품목 */}
             <Section3 />
 
+            <Section8 />
+
             {/* 수거 불가능 품목 */}
             <div className="guideNoCollectionWrap">
+                <img
+                    src="/files/attach/images/sub/x.png"
+                    alt="움직이는 아이콘"
+                    className="moving-icon"
+                />
                 <h3 className="guideSubTitle">수거 불가능 품목</h3>
                 <div className="noCollectionList">
-                    <p><b>하자가 있는 물품:</b> 찢어짐, 큰 오염, 인조 가죽 날림 등</p>
-                    <p><b>기타 불가능 신발류:</b> 바퀴달린 신발, 장화, 고무신 등</p>
-                    <p><b>기타 불가능 이불 및 잡화류:</b> 솜 이불, 토퍼, 라텍스 등</p>
+                    <div className="noCollectionItem">
+                        <b className="noCollectionTitle">하자가 있는 물품:</b>
+                        <div className="noCollectionDivider"></div>
+                        <div className="noCollectionDesc">
+                            찢어짐, 큰 오염, 인조 가죽 날림 등
+                        </div>
+                    </div>
+                    <div className="noCollectionItem">
+                        <b className="noCollectionTitle">기타 불가능 신발류:</b>
+                        <div className="noCollectionDivider"></div>
+                        <div className="noCollectionDesc">
+                            바퀴달린 신발, 장화, 고무신 등
+                        </div>
+                    </div>
+                    <div className="noCollectionItem">
+                        <b className="noCollectionTitle">기타 불가능 이불 및 잡화류:</b>
+                        <div className="noCollectionDivider"></div>
+                        <div className="noCollectionDesc">
+                            솜 이불, 토퍼, 라텍스 등
+                        </div>
+                    </div>
                 </div>
             </div>
 
